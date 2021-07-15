50341ccbc89ab35cee4584b2df24bc3fdc4ed0d7
Josh Fraser <josh_fraser@cyborn.co.uk>
2021-07-15 17:40:03 +0100

    test: add tests for secrets and deferred secrets

--- ionburst_deferred_api_test.go
+++ ionburst_deferred_api_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestDeferredUpload(t *testing.T) {
+func TestDeferredData(t *testing.T) {
 	cli, err := NewClient()
 	if err != nil {
 		t.Error(err)
@@ -75,6 +75,87 @@ func TestDeferredUpload(t *testing.T) {
 			}
 
 			err = cli.Delete(name)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			fmt.Printf("Deleted: %s\n", name)
+
+		}
+
+	}
+
+}
+
+func TestDeferredSecrets(t *testing.T) {
+	cli, err := NewClient()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	a, err := cli.GetClassifications()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	//upload a random payload, get the token and use the token to check on the status (get response)
+
+	fmt.Printf("Classifications: %d\n", len(a))
+
+	name, ba := makeRandomPayload(1024)
+
+	r := bytes.NewReader(ba)
+
+	tk, err := cli.PutDeferredSecrets(name, r, "")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Printf("Uploaded Deferred Secret: %s (%s)\n", name, tk)
+
+	res, err := cli.CheckDeferredSecrets(tk)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !res.Success {
+		t.Error(fmt.Sprintf("ERR: %s - %d", res.Message, res.Status))
+		return
+	} else {
+		fmt.Printf("Uploaded Deferred Secret Success: %s\n", res.ActivityToken)
+
+		tk, err := cli.GetDeferredSecrets(name)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		time.Sleep(5 * time.Second)
+
+		fmt.Printf("Download Deferred Secret: %s (%s)\n", name, tk)
+
+		res, err = cli.CheckDeferredSecrets(tk)
+		if err != nil {
+			t.Error(err)
+			return
+		} else if !res.Success {
+			t.Error(fmt.Sprintf("ERR: %s - %d", res.Message, res.Status))
+			return
+		} else {
+			_, err := cli.FetchDeferredSecrets(tk)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			err = cli.DeleteSecrets(name)
 			if err != nil {
 				t.Error(err)
 				return
